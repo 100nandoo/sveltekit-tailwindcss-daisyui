97e8a0ffa9c1c66b9d1f99f11bc07d0e22d24ecb
Fernando <100nandoo@gmail.com>
2023-03-03 22:11:27 +0800

    Tailwindcss + Daisy UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<h1 class="text-3xl font-bold underline py-8">Hello world!</h1>
+<button class="btn w-64 rounded-full">Button</button>
